46f74743605d1929e5babfc78de9b90adf6476f1
Ahmed Khaled <36384512+AhmedKhaledRe@users.noreply.github.com>
2019-12-28 00:51:18 +0200

    add heroku script / edit server file to deploy / install path library

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const config = require('./config');
 const FakeDb = require('./fake-db');
+const path = require('path');
 
 const messegeRoutes = require('./routes/messeges'),
       userRoutes = require('./routes/users');
@@ -21,8 +22,16 @@ app.use(bodyParser.json());
 app.use('/api/v1/messeges', messegeRoutes);
 app.use('/api/v1/users', userRoutes);
 
-const PORT = process.env.PORT || 5000;
+//serve static assests if in production 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
+  app.get('*', function(req, res) {
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT , function() {
   console.log('App is running!');
 });
