c7c048eb3c154fdc06ba61ea72dead1c396284c6
Ahmed Khaled <36384512+AhmedKhaledRe@users.noreply.github.com>
2019-12-27 23:53:16 +0200

    add login and register page / instal redux-thunk / connect frontent to backend by actions folder / make auth service to authenticate user

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+
+import Header from './components/shared/Header';
+import Login from './components/login/Login';
+import { Register } from './components/register/Register';
+import { LoggedInRoute } from './components/shared/auth/LoggedInRoute';
+
+
+
+import * as actions from './actions';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const store = require('./reducers').init();
+
+class App extends Component {
+
+  componentWillMount() {
+    this.checkAuthState();
+  }
+
+  checkAuthState() {
+    store.dispatch(actions.checkAuthState());
+  }
+
+  logout() {
+    store.dispatch(actions.logout());
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+        <div className='App'>
+          <Header logout={this.logout}/>
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' render={() =>  <Redirect to='/messeges' /> }/>
+              <Route exact path='/login' component={Login} />
+              <LoggedInRoute exact path='/register' component={Register} />
+            </Switch>
+          </div>
+        </div>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/actions/index.js
@@ -0,0 +1,95 @@
+import axios from 'axios';
+import authService from '../services/auth-service';
+
+import { 
+         LOGIN_SUCCESS,
+         LOGIN_FAILURE,
+         LOGOUT,
+         } from './types';
+
+// AUTH ACTIONS ---------------------------
+
+const loginSuccess = () => {
+  const username = authService.getUsername();
+
+  return {
+    type: LOGIN_SUCCESS,
+    username
+  }
+}
+
+const loginFailure = (errors) => {
+  return {
+    type: LOGIN_FAILURE,
+    errors
+  }
+}
+
+export const register = (userData) => {
+  return axios.post('/api/v1/users/register', userData).then(
+    res => res.data,
+    err => Promise.reject(err.response.data.errors)
+  )
+}
+
+export const checkAuthState = () => {
+  return dispatch => {
+    if (authService.isAuthenticated()) {
+      dispatch(loginSuccess());
+    }
+  }
+}
+
+export const login = (userData) => {
+  return dispatch => {
+    return axios.post('/api/v1/users/auth', userData)
+      .then(res => res.data)
+      .then(token => {
+        authService.saveToken(token);
+        dispatch(loginSuccess());
+      })
+      .catch(({response}) => {
+        dispatch(loginFailure(response.data.errors));
+      })
+  }
+}
+
+export const logout = () => {
+  authService.invalidateUser();
+
+  return {
+    type: LOGOUT
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
